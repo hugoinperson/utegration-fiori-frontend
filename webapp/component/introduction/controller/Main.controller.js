3a1d08b99c9fb0171108f7ec80a698ac4d59b487
jhsu <jerry.hsu@utegration.com>
2016-12-26 15:33:31 -0600

    finish slide page nav and routing

--- webapp/component/introduction/controller/Main.controller.js
+++ webapp/component/introduction/controller/Main.controller.js
@@ -1,4 +1,4 @@
-sap.ui.define([
+	sap.ui.define([
 	"jquery.sap.global",
     "sap/ui/core/mvc/Controller"
 ], function($, Controller) {
@@ -8,7 +8,11 @@ sap.ui.define([
 	var CustomController = Controller.extend("utegration.fiori.frontend.component.introduction.controller.Main");
 
 	CustomController.prototype.onInit = function () {
+		// Global variables
 		this._slideNavContainer = this.getView().byId("utg-fiori-SlidePagesNavContainer");
+		this._compRouter = this.getOwnerComponent().getRouter();
+		// Startup functions
+        this._subscribeEvents();
     };
 
     CustomController.prototype.onBeforeRendering = function () {
@@ -16,6 +20,47 @@ sap.ui.define([
     
     CustomController.prototype.onAfterRendering = function () {
     };
+    
+    /*------------------------------------------------------ Events -----------------------------------------------------*/
+
+    CustomController.prototype._subscribeEvents = function () {
+        this._compRouter.getRoute("main").attachPatternMatched(this._onRefresh, this);
+    };
+    
+    CustomController.prototype._onRefresh = function (oEvent) {
+    	var routeName = oEvent.getParameters().name,
+    		slidePageNum = oEvent.getParameters().arguments.slidePageNum;
+    	
+    	if (!slidePageNum) {
+    		this._compRouter.navTo(routeName, {
+    			slidePageNum: "01"
+    		});
+    	} else {
+    		this._navToSlide(slidePageNum);
+    	}
+    };
+    
+    CustomController.prototype._navToSlide = function (slidePageNum) {
+    	var pages = this._slideNavContainer.getPages(),
+    		targetIndex = parseInt(slidePageNum, 10) - 1,
+    		targetPage = pages[targetIndex],
+    		currentPage = this._slideNavContainer.getCurrentPage(),
+    		currentIndex = pages.indexOf(currentPage);
+    		
+		
+		if (targetPage && targetIndex > currentIndex) {
+			this._slideNavContainer.to(targetPage);
+		}
+		
+		if (targetPage && targetIndex < currentIndex) {
+			this._slideNavContainer.backToPage(targetPage);
+		}
+		
+		
+    };
+    
+    
+    
     
     CustomController.prototype.onNextSlide = function () {
     	console.log('nextSlide');	
