307e0f01c17c9328d33b2a3dbe54518190390e96
jhsu <hsuyachieh@gmail.com>
2017-01-08 17:48:00 -0600

    add design fiori apps component

---
+++ webapp/component/designFioriApps/Component.js
@@ -0,0 +1,24 @@
+/*global Promise*/
+sap.ui.define([
+    "jquery.sap.global",
+    "sap/ui/core/UIComponent"
+], function ($, UIComponent) {
+
+    "use strict";
+
+    var CustomComponent = UIComponent.extend("utegration.fiori.frontend.component.designFioriApps.Component", {
+    	metadata: {
+			manifest: "json"
+		}
+    });
+
+    CustomComponent.prototype.init = function () {
+    	// call the base component's init function
+        UIComponent.prototype.init.apply(this);
+        
+        // create the views based on the url/hash
+        this.getRouter().initialize();
+    };
+
+    return CustomComponent;
+});
---
+++ webapp/component/designFioriApps/controller/slides/End.controller.js
@@ -0,0 +1,34 @@
+sap.ui.define([
+	"jquery.sap.global",
+    "utegration/fiori/frontend/model/SlideController"
+], function($, SlideController) {
+	
+	"use strict";
+	
+	var CustomController = SlideController.extend("utegration.fiori.frontend.component.designFioriApps.controller.slides.End");
+
+	CustomController.prototype.onInit = function () {
+		SlideController.prototype.onInit.apply(this, arguments);
+		
+		// Global variables
+		this._coreEventBus = sap.ui.getCore().getEventBus();
+    };
+
+    CustomController.prototype.onBeforeRendering = function () {
+    	SlideController.prototype.onBeforeRendering.apply(this, arguments);
+    };
+    
+    CustomController.prototype.onAfterRendering = function () {
+    	SlideController.prototype.onAfterRendering.apply(this, arguments);
+    };
+    
+    CustomController.prototype.onExit = function () {
+        SlideController.prototype.onExit.apply(this, arguments);
+    };
+
+	CustomController.prototype.onNextTopic = function () {
+		this._coreEventBus.publish("app", "navToTopic", {topic: "topic-history"});
+	};
+
+    return CustomController;
+});
